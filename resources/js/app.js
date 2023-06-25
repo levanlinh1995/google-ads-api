27fd377e7ca0ce8727982198ce7cb2953ded826e
Robert <levanlinh1995@gmail.com>
2023-06-25 14:53:28 +0700

    install and setup vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import '../css/app.css'; 
+
+import {createApp} from 'vue'
+
+createApp().mount('#app')
---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Google Ads API</title>
+    </head>
+    <body>
+        <div id="app">
+            <h1>hello</h1>
+        </div>
+
+        @vite('resources/js/app.js')
+    </body>
+</html>
